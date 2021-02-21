3e2a96509e2acc770d59f4cf32ae0b97a3b9d8ca
kryom-arc <67024644+kryom-arc@users.noreply.github.com>
2021-02-20 21:16:39 -0300

    created quem-somos page

---
+++ quemSomos.js
@@ -0,0 +1,16 @@
+// header scroll function ///////////////////////////////////////////////////////////////////////////////////////
+document.body.onscroll = () => {
+    let header = document.querySelector("header .header");
+    let logoArea = document.querySelector('.header .logo-area');
+    if (window.pageYOffset > 200) {
+        header.style.backgroundColor = '#9E6583';
+        logoArea.style.height = '90px';
+        logoArea.style.width = '140px';
+        header.style.height = '100px';
+    } else {
+        header.style.backgroundColor = '';
+        logoArea.style.height = '';
+        logoArea.style.width = '';
+        header.style.height = '';
+    }
+}
